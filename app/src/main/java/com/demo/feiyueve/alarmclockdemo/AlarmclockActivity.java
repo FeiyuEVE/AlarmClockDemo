72d368f1adf802e228dc63a090f6862ef0f55b77
FeiyuEVE <765031220@qq.com>
2017-09-15 23:28:14 +0800

    Initial commit
    建立完所有activity

---
+++ app/src/main/java/com/demo/feiyueve/alarmclockdemo/AlarmclockActivity.java
@@ -0,0 +1,13 @@
+package com.demo.feiyueve.alarmclockdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmclockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarmclock);
+    }
+}
