ce24d1705d4203d7329b037523b41eba2f19b8ae
FeiyuEVE <765031220@qq.com>
2017-09-19 01:05:23 +0800

    日常更新

--- app/src/main/java/com/demo/feiyueve/alarmclockdemo/AddAlarmActivity.java
+++ app/src/main/java/com/demo/feiyueve/alarmclockdemo/AddAlarmActivity.java
@@ -4,9 +4,11 @@ package com.demo.feiyueve.alarmclockdemo;
  * Created by FeiyuEVE on 2017/9/17.
  */
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -17,7 +19,7 @@ import java.util.Calendar;
 public class AddAlarmActivity extends AppCompatActivity {
 
     private TimePicker timePicker;
-    private Button bt_ok,bt_empty;
+    private Button bt_ok;
     private SharedPreferences.Editor timeEditor;
     private SharedPreferences alarmTime;;
 
@@ -27,22 +29,23 @@ public class AddAlarmActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_alarm);
         timePicker = (TimePicker) findViewById(R.id.timePicker);
         timePicker.setIs24HourView(true);
-        bt_empty = (Button) findViewById(R.id.bt_empty);
         bt_ok = (Button) findViewById(R.id.bt_ok);
         timeEditor = getSharedPreferences("alarmTime",MODE_PRIVATE).edit();
         alarmTime = getSharedPreferences("alarmTime",MODE_PRIVATE);
 
-        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+        timeEditor.putInt("alarmTime_Hour"+0,-1);
+
+        bt_ok.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onTimeChanged(TimePicker timePicker, int hourOfDay, int minuteOfHour) {
-                for(int i=0;i<10;i++){
-                    if(alarmTime.getInt("alarmTime_Hour"+i,-1)!=-1){
-                        if(alarmTime.getInt("alarmTime_Minute"+i,-1)!=-1){
+            public void onClick(View view) {
+                timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+                @Override
+                public void onTimeChanged(TimePicker timePicker, int hourOfDay, int minuteOfHour) {
+                    for(int i=0;i<10;i++){
+                        if(alarmTime.getInt("alarmTime_Hour"+i,-1)!=-1&&alarmTime.getInt("alarmTime_Minute"+i,-1)!=-1){
                             continue;
                         }
-                    }
-                    if(alarmTime.getInt("alarmTime_Hour"+i,-1)==-1) {
-                        if(alarmTime.getInt("alarmTime_Minute"+i,-1)==-1){
+                        if(alarmTime.getInt("alarmTime_Hour"+i,-1)==-1||alarmTime.getInt("alarmTime_Minute"+i,-1)==-1) {
                             timeEditor.putInt("alarmTime_Hour"+i,hourOfDay);
                             timeEditor.putInt("alarmTime_Minute"+i,minuteOfHour);
                             timeEditor.apply();
@@ -51,6 +54,10 @@ public class AddAlarmActivity extends AppCompatActivity {
                         }
                     }
                 }
+            });
+                Intent intent = new Intent();
+                intent.setClass(AddAlarmActivity.this,MainActivity.class);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/demo/feiyueve/alarmclockdemo/MainActivity.java
+++ app/src/main/java/com/demo/feiyueve/alarmclockdemo/MainActivity.java
@@ -7,11 +7,9 @@ package com.demo.feiyueve.alarmclockdemo;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.provider.AlarmClock;
-import android.provider.Telephony;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -26,40 +24,53 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.TimeZone;
 
+import static android.R.attr.format;
+
 public class MainActivity extends AppCompatActivity{
 
     private ArrayAdapter<String> adapter;
+    private SharedPreferences.Editor timeEditor;
     private SharedPreferences sharedPreferences;
     private ArrayList<String> alarmList = new ArrayList<String>();
-    private TimePicker timePicker;
     private ListView listView;
-    private String city;
-    private Calendar mCalendar = Calendar.getInstance();
+    private Calendar mCalendar,newCalendar;
     private AlarmManager alarmManager;
-    PendingIntent pendingIntent;
+    private PendingIntent pendingIntent;
+    private long firstTime,systemTime,alarmClock,time;
+    private Intent alarmClockIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        firstTime = SystemClock.elapsedRealtime();
+        systemTime = System.currentTimeMillis();
+
+
         Button bt_addAlarm = (Button) findViewById(R.id.bt_addAlarm);
         Button bt_weather = (Button) findViewById(R.id.bt_addWeather);
         Button bt_stopWatch = (Button) findViewById(R.id.bt_stopWatch);
         listView = (ListView) findViewById(R.id.listView);
-        timePicker = (TimePicker) findViewById(R.id.timePicker);
+
+        mCalendar = Calendar.getInstance();
         mCalendar.setTimeInMillis(System.currentTimeMillis());
         mCalendar.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+
+        timeEditor = getSharedPreferences("alarmTime",MODE_PRIVATE).edit();
+
         sharedPreferences = getSharedPreferences("alarmTime",MODE_PRIVATE);
-        Intent alarmClockIntent = new Intent(MainActivity.this, AlarmClock.class);
+
+        alarmClockIntent = new Intent(MainActivity.this, AlarmReceiver.class);
         pendingIntent = PendingIntent.getBroadcast(MainActivity.this,0,alarmClockIntent,0);
         alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
 
-
+        checkTime();
         resetAlarmList();
 
+
         adapter = new ArrayAdapter<String>
-                (this,android.R.layout.simple_list_item_multiple_choice,alarmList);
+                (this,android.R.layout.simple_list_item_1);
         listView.setAdapter(adapter);
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
                @Override
@@ -67,9 +78,25 @@ public class MainActivity extends AppCompatActivity{
                {
                    alarmList.remove(i);
                    adapter.notifyDataSetChanged();
+                   for(int j=0;j<10;j++) {
+                       newCalendar = Calendar.getInstance();
+                       newCalendar.setTimeInMillis(System.currentTimeMillis());
+                       mCalendar.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+                       newCalendar.set(Calendar.HOUR_OF_DAY, sharedPreferences.getInt("alarmTime_Hour" + j, -1));
+                       newCalendar.set(Calendar.MINUTE, sharedPreferences.getInt("alarmTime_Minute" + j, -1));
+                       alarmClock = newCalendar.getTimeInMillis();
+                       if (alarmClock < systemTime) {
+                           timeEditor.putInt("alarmTime_Hour"+i,-1);
+                           timeEditor.putInt("alarmTime_Minute"+i,-1);
+                           timeEditor.apply();
+                       }
+                   }
+                   Toast.makeText(MainActivity.this,"该闹钟已删除",Toast.LENGTH_LONG);
                    return true;
                }
         });
+
+
 
         //打开添加闹钟
         bt_addAlarm.setOnClickListener(new View.OnClickListener() {
@@ -103,20 +130,26 @@ public class MainActivity extends AppCompatActivity{
     public void resetAlarmList(){
         for(int i=0;i<10;i++)
         {
-            alarmList.add(sharedPreferences.getInt("alarmTime_Hour"+i,-1)+":"+sharedPreferences.getInt("alarmTime_Minute"+i,-1));
+            alarmList.add(String.valueOf(newCalendar));
         }
     }
 
     public void checkTime(){
         for(int i=0;i<10;i++){
-            Calendar newCalendar = Calendar.getInstance();
+            newCalendar = Calendar.getInstance();
             newCalendar.setTimeInMillis(System.currentTimeMillis());
-            newCalendar.set(Calendar.HOUR,sharedPreferences.getInt("alarmTime_Hour"+i,-1));
+            mCalendar.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+            newCalendar.set(Calendar.HOUR_OF_DAY,sharedPreferences.getInt("alarmTime_Hour"+i,-1));
             newCalendar.set(Calendar.MINUTE,sharedPreferences.getInt("alarmTime_Minute"+i,-1));
-            if(newCalendar.compareTo(mCalendar)==1){
-                AlarmManager manager = (AlarmManager)getSystemService(ALARM_SERVICE);
-                manager.set(AlarmManager.RTC_WAKEUP, mCalendar.getTimeInMillis(), pendingIntent);
+            alarmClock = newCalendar.getTimeInMillis();
+            if(alarmClock<systemTime){
+                newCalendar.add(Calendar.DAY_OF_MONTH, 1);
+                alarmClock = newCalendar.getTimeInMillis();
             }
+            time = alarmClock - systemTime;
+            firstTime += time;
+
+            alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,firstTime,AlarmManager.INTERVAL_DAY,pendingIntent);
         }
     }
 }
